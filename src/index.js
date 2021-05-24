df2c7ef5b1911f1e052c81d0123b30f6f5c08247
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-05-24 11:41:31 +0500

    Basic GraphQL Project

---
+++ src/index.js
@@ -0,0 +1,190 @@
+import { GraphQLServer } from "graphql-yoga";
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Demo user data
+const users = [
+  {
+    id: "1",
+    name: "Andrew",
+    email: "andrew@example.com",
+    age: 27,
+  },
+  {
+    id: "2",
+    name: "Sarah",
+    email: "sarah@example.com",
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "mike@example.com",
+  },
+];
+
+const posts = [
+  {
+    id: "10",
+    title: "GraphQL 101",
+    body: "This is how to use GraphQL...",
+    published: true,
+    author: "1",
+  },
+  {
+    id: "11",
+    title: "GraphQL 201",
+    body: "This is an advanced GraphQL post...",
+    published: false,
+    author: "1",
+  },
+  {
+    id: "12",
+    title: "Programming Music",
+    body: "",
+    published: false,
+    author: "2",
+  },
+];
+
+const comments = [
+  {
+    id: "1",
+    text: "This is a new comment",
+    author: "1",
+    post: "10",
+  },
+  {
+    id: "2",
+    text: "Comm new wow",
+    author: "1",
+    post: "11",
+  },
+  {
+    id: "3",
+    text: "Hello world!",
+    author: "2",
+    post: "12",
+  },
+  {
+    id: "4",
+    text: "Great tutorial!",
+    author: "3",
+    post: "10",
+  },
+];
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        comments: [Comment!]!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    comments(parent, args, ctx, info) {
+      return [...comments];
+    },
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
+    },
+    me() {
+      return {
+        id: "123098",
+        name: "Mike",
+        email: "mike@example.com",
+      };
+    },
+    post() {
+      return {
+        id: "092",
+        title: "GraphQL 101",
+        body: "",
+        published: false,
+      };
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => comment.post === parent.id);
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => post.author === parent.id);
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => comment.author === parent.id);
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => user.id === parent.author);
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => post.id === parent.post);
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log("The server is up!");
+});
