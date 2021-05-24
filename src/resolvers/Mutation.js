cb5005d97d130b87df5de655bc309d7f3f16f61d
Bilal Afzal <bilal@shiftcrowd.co.uk>
2021-05-24 17:38:43 +0500

    Added Comments Subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -150,20 +150,31 @@ const Mutation = {
       id: uuidv4(),
     };
     db.comments.push(comment);
-    pubsub.publish(`comment ${args.data.post}`, { comment });
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: "CREATED",
+        data: comment,
+      },
+    });
     return comment;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       (comment) => comment.id === args.id
     );
     if (commentIndex === -1) {
       throw new Error("No such comment exists");
     }
-    const deletedComment = db.comments.splice(commentIndex, 1);
-    return deletedComment[0];
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: "DELETED",
+        data: deletedComment,
+      },
+    });
+    return deletedComment;
   },
-  updateComment(parent, args, { db }, info) {
+  updateComment(parent, args, { db, pubsub }, info) {
     const { id, data } = args;
     const comment = db.comments.find((comment) => comment.id === id);
     if (!comment) {
@@ -172,6 +183,12 @@ const Mutation = {
     if (typeof data.text === "string" && data.text) {
       comment.text = data.text;
     }
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: "UPDATED",
+        data: comment,
+      },
+    });
     return comment;
   },
 };
