a4ab54f0ce9fbdaf2c883638e733cdb3b163511f
bilalafzal01 <mafzal.bscs18seecs@seecs.edu.pk>
2021-05-24 12:31:16 +0500

    added mutation input types

--- src/index.js
+++ src/index.js
@@ -84,9 +84,28 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author: ID!,post: ID!): Comment!
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput{
+      name: String!
+      email: String!
+      age: Int
+    }
+
+    input CreatePostInput{
+      title: String!
+      body: String!
+      published: Boolean!
+      author: ID!
+    }
+
+    input CreateCommentInput{
+      text: String!
+      author: ID!
+      post: ID!
     }
 
     type User {
@@ -162,32 +181,32 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some((user) => user.email === args.email);
+      const emailTaken = users.some((user) => user.email === args.data.email);
       if (emailTaken) {
         throw new Error("Email already taken");
       }
       const user = {
-        ...args,
+        ...args.data,
         id: uuidv4(),
       };
       users.push(user);
       return user;
     },
     createPost(parent, args, ctx, info) {
-      const userExists = users.some((user) => user.id === args.author);
+      const userExists = users.some((user) => user.id === args.data.author);
       if (!userExists) {
         throw new Error("No such author exists");
       }
       const post = {
-        ...args,
+        ...args.data,
         id: uuidv4(),
       };
       posts.push(post);
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExists = users.some((user) => user.id === args.author);
-      const postExists = posts.some((post) => post.id === args.post);
+      const userExists = users.some((user) => user.id === args.data.author);
+      const postExists = posts.some((post) => post.id === args.data.post);
       if (!userExists) {
         throw new Error("No such user exists");
       }
@@ -195,7 +214,7 @@ const resolvers = {
         throw new Error("No such post exists");
       }
       const comment = {
-        ...args,
+        ...args.data,
         id: uuidv4(),
       };
       comments.push(comment);
